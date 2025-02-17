e8d14a31983763e0b4ec46a709afbaa684e5fb51
JuniPia <junyong0801@naver.com>
2025-02-17 18:34:36 +0900

    commit

--- src/main/java/kr/co/iei/board/controller/BoardController.java
+++ src/main/java/kr/co/iei/board/controller/BoardController.java
@@ -2,10 +2,18 @@ package kr.co.iei.board.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 @Controller
+
 public class BoardController {
 	@GetMapping(value="/boardList")
 	public String boardList() {
 		return "board/boardList";
+	}
+	
+	@GetMapping(value="/board")
+	public String boardWrtie() {
+		return "board/board";
 	}
 }
---
+++ src/main/java/kr/co/iei/board/service/BoardService.java
@@ -0,0 +1,12 @@
+package kr.co.iei.board.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kr.co.iei.board.dao.BoardDao;
+
+@Service
+public class BoardService {
+	@Autowired
+	private BoardDao boardDao;
+}
