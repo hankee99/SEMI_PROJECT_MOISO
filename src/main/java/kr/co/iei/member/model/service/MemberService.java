457250e533e06400e8f3cc97cc8fd3b475c2d952
hongbee-coder <congbii94@gmail.com>
2025-02-24 02:51:47 +0900

    프로필사진구현

--- src/main/java/kr/co/iei/member/model/service/MemberService.java
+++ src/main/java/kr/co/iei/member/model/service/MemberService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 import kr.co.iei.member.model.dao.MemberDao;
 import kr.co.iei.member.model.vo.Member;
@@ -49,6 +50,13 @@ public class MemberService {
 	public List selectSigungu(String sido) {
 		List list = memberDao.selectSigungu(sido);
 		return list;
+	}
+
+	
+	@Transactional
+	public int updateMypage(Member loginUser, MultipartFile upfile) {
+		int result = memberDao.updateMypage(loginUser, upfile);
+		return result;
 	}
 
 	
