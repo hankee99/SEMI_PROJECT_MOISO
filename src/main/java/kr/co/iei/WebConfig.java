1086c7f37804c4d70acf75d631d6de2d32ae8d3c
hankee99 <hankee60@gmail.com>
2025-02-25 23:05:01 +0900

    25일 마지막 커밋

--- src/main/java/kr/co/iei/WebConfig.java
+++ src/main/java/kr/co/iei/WebConfig.java
@@ -7,6 +7,8 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import kr.co.iei.util.LoginInterceptor;
+
 //스프링부트 설정파일임을 나타내는 어노테이션
 @Configuration
 @EnableWebMvc
@@ -35,4 +37,23 @@ public class WebConfig implements WebMvcConfigurer{
 		.addResourceHandler("/moisoPhoto/editor/**")
 		.addResourceLocations("file:///"+root+"/moisoPhoto/editor/");
 	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new LoginInterceptor())
+		.addPathPatterns("/group/groupBoardType",
+						"/group/writeFrm",
+						"/group/newGroup",
+						"/board/boardWriteFrm",
+						"/board/boardUpdateFrm",
+						"/group/groupBoard",
+						"/member/**")
+		.excludePathPatterns("/member/loginMsg",
+								"/member/loginFrm",
+								"/member/login"
+				);
+		
+	}
+	
+	
 }
